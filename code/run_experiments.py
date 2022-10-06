83060cedcf9d8a67fcb130761909ac46c22bff88
Mihai Constantinov <97915789+ConstantinovMihai@users.noreply.github.com>
2022-10-06 14:16:02 +0200

    refactored the code. added the coefficient of variation technique

--- code/run_experiments.py
+++ code/run_experiments.py
@@ -8,184 +8,16 @@ Note: To make the animation work in Spyder you should set graphics backend to 'A
 import argparse
 from cProfile import run
 import glob
-from pathlib import Path
-from cbs import CBSSolver
-from independent import IndependentSolver
-from prioritized import PrioritizedPlanningSolver
 from distributed import DistributedPlanningSolver # Placeholder for Distributed Planning
 from visualize import Animation
 from single_agent_planner import get_sum_of_cost
-from create_sim2 import generatesSimulation
+from create_sim2 import createsSimulationInput
 import numpy as np
 import matplotlib.pyplot as plt
 import pickle
+import utilities
 
 SOLVER = "CBS"
-
-def print_mapf_instance(my_map, starts, goals):
-    """
-    Prints start location and goal location of all agents, using @ for an obstacle, . for a open cell, and 
-    a number for the start location of each agent.
-    
-    Example:
-        @ @ @ @ @ @ @ 
-        @ 0 1 . . . @ 
-        @ @ @ . @ @ @ 
-        @ @ @ @ @ @ @ 
-    """
-    print('Start locations')
-    print_locations(my_map, starts)
-    print('Goal locations')
-    print_locations(my_map, goals)
-
-def print_locations(my_map, locations):
-    """
-    See docstring print_mapf_instance function above.
-    """
-    starts_map = [[-1 for _ in range(len(my_map[0]))] for _ in range(len(my_map))]
-    for i in range(len(locations)):
-        starts_map[locations[i][0]][locations[i][1]] = i
-    to_print = ''
-    for x in range(len(my_map)):
-        for y in range(len(my_map[0])):
-            if starts_map[x][y] >= 0:
-                to_print += str(starts_map[x][y]) + ' '
-            elif my_map[x][y]:
-                to_print += '@ '
-            else:
-                to_print += '. '
-        to_print += '\n'
-    print(to_print)
-
-def import_mapf_instance(filename):
-    """
-    Imports mapf instance from instances folder. Expects input as a .txt file in the following format:
-        Line1: #rows #columns (number of rows and columns)
-        Line2-X: Grid of @ and . symbols with format #rows * #columns. The @ indicates an obstacle, whereas . indicates free cell.
-        Line X: #agents (number of agents)
-        Line X+1: xCoordStart yCoordStart xCoordGoal yCoordGoal (xy coordinate start and goal for Agent 1)
-        Line X+2: xCoordStart yCoordStart xCoordGoal yCoordGoal (xy coordinate start and goal for Agent 2)
-        Line X+n: xCoordStart yCoordStart xCoordGoal yCoordGoal (xy coordinate start and goal for Agent n)
-        
-    Example:
-        4 7             # grid with 4 rows and 7 columns
-        @ @ @ @ @ @ @   # example row with obstacle in every column
-        @ . . . . . @   # example row with 5 free cells in the middle
-        @ @ @ . @ @ @
-        @ @ @ @ @ @ @
-        2               # 2 agents in this experiment
-        1 1 1 5         # agent 1 starts at (1,1) and has (1,5) as goal
-        1 2 1 4         # agent 2 starts at (1,2) and has (1,4) as goal
-    """
-    f = Path(filename)
-    if not f.is_file():
-        raise BaseException(filename + " does not exist.")
-    f = open(filename, 'r')
-    # first line: #rows #columns
-    line = f.readline()
-    rows, columns = [int(x) for x in line.split(' ')]
-    rows = int(rows)
-    columns = int(columns)
-    # #rows lines with the map
-    my_map = []
-    for r in range(rows):
-        line = f.readline()
-        my_map.append([])
-        for cell in line:
-            if cell == '@':
-                my_map[-1].append(True)
-            elif cell == '.':
-                my_map[-1].append(False)
-    # #agents
-    line = f.readline()
-    num_agents = int(line)
-    # #agents lines with the start/goal positions
-    starts = []
-    goals = []
-    for a in range(num_agents):
-        line = f.readline()
-        sx, sy, gx, gy = [int(x) for x in line.split(' ')]
-        starts.append((sx, sy))
-        goals.append((gx, gy))
-    f.close()
-
-    return my_map, starts, goals
-
-
-def processArgs(args, my_map, starts, goals ):
-
-    time = 0
-    paths = []
-
-    if args.solver == "CBS":
-        print("***Run CBS***")
-        cbs = CBSSolver(my_map, starts, goals)
-        paths, time = cbs.find_solution(args.disjoint)
-    elif args.solver == "Independent":
-        print("***Run Independent***")
-        solver = IndependentSolver(my_map, starts, goals)
-        paths, time = solver.find_solution()
-    elif args.solver == "Prioritized":
-        print("***Run Prioritized***")
-        solver = PrioritizedPlanningSolver(my_map, starts, goals)
-        paths, time = solver.find_solution()
-    elif args.solver == "Distributed":  # Wrapper of distributed planning solver class
-        print("***Run Distributed Planning***")
-        solver = DistributedPlanningSolver(my_map, starts, goals, ...) #!!!TODO: add your own distributed planning implementation here.
-        paths, time = solver.find_solution()
-    else: 
-        raise RuntimeError("Unknown solver!")
-
-    return paths, time
-
-
-def runSimulation(args, results, saveCVS = False): # NO LONGER USED
-    """Runs the simulation using the provided args and either stores the data or puts in the results dictionary 
-
-    Args:
-        args (list): the arguments from the command line
-        results (list, optional): stores the total cost and the time needed to run a particular experiment. Defaults to {}.
-        saveCVS (bool, optional): if set to true it will write the results in a CVS file. Defaults to False.
-    """
-    result_file = open("results.csv", "w", buffering=1)
-
-    # iterates among all the files 
-    for file in sorted(glob.glob(args.instance)):
-
-        print("***Import an instance***")
-        #my_map, starts, goals = import_mapf_instance(file)
-        my_map, starts, goals = generatesSimulation(0,3,0)
-        print_mapf_instance(my_map, starts, goals)
-
-        # get paths and time for the simulation
-        paths, time = processArgs(args, my_map, starts, goals )
-
-        # computes the total cost
-        cost = get_sum_of_cost(paths)
-
-        # if saveCVS is set to true write in the a CVS file
-        if saveCVS:
-            result_file.write("{},{},{}\n".format(file, cost, round(time, 6)))
-        
-
-        # process the file key to get file_key: first element contains map-agent-type, the second elements contains the index
-        file_key = ((file.split("/")[1]).split(".")[0]).split("-idx_")
-        
-        # check if the key is already in the dict, if not, create it
-        if file_key[0] not in results:
-            results[file_key[0]] = np.array((cost, round(time, 6)))
-        # add the simulation results to the corresponding dict
-        else:
-            results[file_key[0]] = np.append(results[file_key[0]], (cost, round(time, 6)))
-
-        if not args.batch:
-            print("***Test paths on a simulation***")
-            animation = Animation(my_map, starts, goals, paths)
-            # animation.save("output.mp4", 1.0) # install ffmpeg package to use this option
-            animation.show()
-
-    result_file.close()
-    
 
 def testPathSimulation(args, my_map, starts, goals, paths):
     if not args.batch:
@@ -196,7 +28,7 @@ def testPathSimulation(args, my_map, starts, goals, paths):
 
 
 def runOneExperiment(map, agent, spawn_type, results):
-    """ Runs on experiment for a certain instance
+    """ Runs on experiment for a certain instance (i.e a certain map-nb_agents-spawn_type combination)
 
     Args:
         map (_type_): _description_
@@ -204,10 +36,10 @@ def runOneExperiment(map, agent, spawn_type, results):
         spawn_type (_type_): _description_
         results (_type_): _description_
     """
-    my_map, starts, goals = generatesSimulation(map, agent, spawn_type)
+    my_map, starts, goals = createsSimulationInput(map, agent, spawn_type)
                     
     # get paths and time for the simulation
-    paths, time = processArgs(args, my_map, starts, goals )
+    paths, time = utilities.processArgs(args, my_map, starts, goals )
 
     # computes the total cost
     cost = get_sum_of_cost(paths)
@@ -224,103 +56,83 @@ def runOneExperiment(map, agent, spawn_type, results):
     testPathSimulation(args, my_map, starts, goals, paths)
 
 
-def generateExperiments(nb_maps, max_agents, nb_spawns, results, args):
-    """_summary_
+def generateExperiments(nb_maps, max_agents, nb_spawns, results, args, min_agents = 2, min_map = 0):
+    """ Generates experiments for all possible combinations of maps, nb of agents and spawn types until
+        the coefficient of variation stabilises  
 
     Args:
-        nb_maps (_type_): _description_
-        max_agents (_type_): _description_
-        nb_spawns (_type_): _description_
-        results (_type_): description
-        args (_type_): _description_
+        nb_maps (int): maximum number of maps in all simulations
+        max_agents (int): maximum number of agents in all the simulations
+        nb_spawns (int): maximum number of spawn types (usually, there will be only 2 options)
+        results (dict): stores the results for the iterations for each experimental instance
+        args (string): command line arguments
+        min_agents (int) : minimum number of agents in all the simulations
+        min_map (int) : minimum number of maps in all simulations (for debugging purposes)
     """
-    for map in range(nb_maps):
-        for agent in range(2, max_agents):
+
+    # iterates among all the maps, agents and spawn types
+    for map in range(min_map, nb_maps):
+        for agent in range(min_agents, max_agents+1):
             for spawn_type in range(nb_spawns):
-                # TODO: IMPLEMENT THE STATISTICAL METHODS HERE
+                # name of the key in the results dict
                 key = f"map_{map}-agent_{agent}-spawn_{spawn_type}"
+
+                # avoids "referenced before assignment error"
                 variation_cost = [0]
                 variation_time = [0]
 
                 # condition for stopping to iterate:
                 # if the current variation coefficient is within a half of a standard deviation from
                 # the mean of the last quarter of the simulations
-                while (len(variation_time) < 50 or (np.std(variation_time[-30:]) < 0.5 * np.mean(variation_time[-30:]))
-                    or (np.std(variation_cost[-30:]) < 0.5 * np.mean(variation_cost[-30:]))):
-                    print(f"len(variation_time) IS {len(variation_time)}")
+                while ((np.std(variation_time[-100:]) >= 0.25 * np.mean(variation_time[-100:]))
+                    or (np.std(variation_cost[-100:]) >= 0.25 * np.mean(variation_cost[-100:]))):
+                    
+                    # runs one experiment
                     runOneExperiment(map, agent, spawn_type, results)
+                    
                     data = results[key]
-                    variation_cost = data[::2]
-                    variation_time = data[1::2]
+
+                    # find the indeces where cost is 0 (the valid experiments)
+                    # store their indeces
+                    valid_experiments = np.nonzero(data[::2])
+
+                    # store the costs and times obtained in the valid experiments
+                    valid_costs = data[::2][valid_experiments]
+                    valid_times = data[1::2][valid_experiments]
+
                     # the variation coefficient = standard deviation / mean
-                    variation_cost = np.append(variation_cost, np.std(data[::2]) / np.mean(data[::2]))
-                    variation_time = np.append(variation_time, np.std(data[1::2]) / np.mean(data[1::2]))
-                    if (len(variation_time) == 150):
+                    # those are to be computed for the entire data set
+                    variation_cost = np.append(variation_cost, np.std(valid_costs) / np.mean(valid_costs))
+                    variation_time = np.append(variation_time, np.std(valid_times) / np.mean((valid_times)))
+                    
+                    if (len(variation_time) == 200):
                         break
-
-                print(f"len of variation is {len( variation_time)}")
+                
+                    # TODO: remove this later when we're sure this routine works as intended
+                    print(f"len of variation is {len( variation_time)}")
                 x = np.arange(len(variation_time))
-                plt.plot(x, variation_time)
-                plt.xlabel("iteration")
-                plt.ylabel("coefficient of variation for time")
+
+                # plot of the costs&times
+                plt.plot(x, variation_cost, color="red", label="cost")
+                plt.plot(x, variation_time, color="green", label="time")
+                plt.ylim(bottom=0)
+                plt.legend()
                 plt.show()
-
-
-
-
-def parseArgs():
-    parser = argparse.ArgumentParser(description='Runs various MAPF algorithms')
-    parser.add_argument('--instance', type=str, default=None,
-                        help='The name of the instance file(s)')
-    parser.add_argument('--batch', action='store_true', default=False,
-                        help='Use batch output instead of animation')
-    parser.add_argument('--disjoint', action='store_true', default=False,
-                        help='Use the disjoint splitting')
-    parser.add_argument('--solver', type=str, default=SOLVER,
-                        help='The solver to use (one of: {CBS,Independent,Prioritized}), defaults to ' + str(SOLVER))
-
-    args = parser.parse_args()
-    return args
-
-
-def processResults(results):
-    for key in results.keys():
-        data = results[key]
-
-        # python slicing: start - stop -step
-        costs = data[::2]
-      
-        times = data[1::2]
-        plotData(times)
-
-
-def plotData(data):
-    """ Plotting routine
-
-    Args:
-        data (_type_): _description_
-    """
-    #TODO: Improve
-
-    plt.hist(data)
-    plt.show()
 
 
 def runSimulation(args):
     """ Runs the experiments and saves the results in a pickle structure
     """
 
-    generateExperiments(nb_maps=2, max_agents=3, nb_spawns=1, results=results, args=args)
+    generateExperiments(nb_maps=2, max_agents=3, nb_spawns=1, min_agents=3, args=args, results=results)
     # save the dicionary
     with open('saved_dictionary.pkl', 'wb') as f:
         pickle.dump(results, f)
 
 
 if __name__ == '__main__':
-
-    # generateExperiments(nb_maps=2, max_agents=3, nb_spawns=1)
-    
-    args = parseArgs()
+        
+    args = utilities.parseArgs()
     results = {}
 
     runSimulation(args)
@@ -329,9 +141,3 @@ if __name__ == '__main__':
     with open('saved_dictionary.pkl', 'rb') as f:
         results = pickle.load(f)
 
-    
-    # print(results.keys())
-
-    processResults(results)
-
-    
