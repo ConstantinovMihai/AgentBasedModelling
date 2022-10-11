fd438ede9603b2b2ba3227b3c1414e78fc4c67a1
Mihai Constantinov <97915789+ConstantinovMihai@users.noreply.github.com>
2022-10-11 12:08:09 +0200

    function which checks whether or not the agents reached their locations is implemented

--- code/distributed.py
+++ code/distributed.py
@@ -10,6 +10,75 @@ from aircraft import AircraftDistributed
 from cbs import detect_collision, detect_collisions
 
 class DistributedPlanningSolverIndividual(object):
+    """A distributed planner where agents do not communicate with each other"""
+
+    def __init__(self, my_map, starts, goals):
+        """my_map   - list of lists specifying obstacle positions
+        starts      - [(x1, y1), (x2, y2), ...] list of start locations
+        goals       - [(x1, y1), (x2, y2), ...] list of goal locations
+        """
+        self.CPU_time = 0
+        self.my_map = my_map
+        self.starts = starts
+        self.goals = goals
+        self.num_of_agents = len(goals)
+        self.heuristics = [] 
+        self.radar_radius = 5
+        # the locations of all agents
+        self.locations = starts
+
+        for goal in self.goals:
+            self.heuristics.append(compute_heuristics(my_map, goal))
+
+        self.time = 0 # this is going to incrementaly increase and decisions are going to be made at each timestep
+    
+    def goalsReached(self):
+        """ Checks whether the agents reached their goals
+            Returns a lists of bools 
+        """
+        reached = []
+        for idx, ag in enumerate(self.locations):
+            reached.append(ag == self.goals[idx])
+
+        return reached
+
+
+    def find_solution(self):
+        """ Finds paths for all agents from their start locations to their goal locations."""
+
+        start_time = timer.time()
+        # stores all the paths
+        
+        # in the prioritized planning, the entire path was compute at once
+        # now, for each time step we should 1. update the constraint list for the next timestep; use the radar
+        # check if new constraints were added and then find the next location using A*
+        
+        # this stores the paths for each agent (this time we'll append the results with one location at a time)
+        result = []
+        
+        # stopping criterion for the simulation
+        # iterates over time
+        # iterates among all the agents 
+        # setting up the bubble constraints
+        
+   
+
+        for i in range(self.num_of_agents):  # Find path for each agent
+            path = a_star(self.my_map, self.starts[i], self.goals[i], self.heuristics[i],
+                          i, [])
+            if path is None:
+                raise BaseException('No solutions')
+            result.append(path)
+
+
+        self.CPU_time = timer.time() - start_time
+
+        print("\n Found a solution! \n")
+        print("CPU time (s):    {:.2f}".format(self.CPU_time))
+        print("Sum of costs:    {}".format(get_sum_of_cost(result)))
+
+        return result, self.CPU_time
+
 
 class DistributedPlanningSolver(object):
     """A distributed planner"""
