8d1525220047fe6e1bdbcb6ee3134e60b024232e
Mihai Constantinov <97915789+ConstantinovMihai@users.noreply.github.com>
2022-11-20 16:14:37 +0100

    WIP

--- code/statistics.py
+++ code/statistics.py
@@ -15,7 +15,8 @@ def getTimeFailuresArray(filename):
         results = pickle.load(f)[0]
 
     # stores the simulation time
-    times = np.zeros(11)
+    times1 = np.zeros(11)
+    times2 = np.zeros(11)
     failures = np.zeros(11)
 
     # total number of agents
@@ -28,38 +29,47 @@ def getTimeFailuresArray(filename):
         # contains map, nb agents, spawn type
         
         nb_agents = int(re.findall(r'\d+', exp.split("-")[1])[0])
-        if not np.isnan(exp_results['mean_time']):
-            nb_valid_exp[nb_agents] += 1
-            times[nb_agents] += exp_results['mean_time']
-            failures[nb_agents] += exp_results['failed_perc']
-            costs_arr.append(exp_results['mean_cost'])
+        spawn_type = int(re.findall(r'\d+', exp.split("-")[2])[0])
+        
 
-    return times[1:] / nb_valid_exp[1:], failures[1:] / nb_valid_exp[1:] * 100, nb_agents_arr, costs_arr
+        if not np.isnan(exp_results['mean_time']):
+            if spawn_type == 1:
+                nb_valid_exp[nb_agents] += 1
+                times1[nb_agents] += exp_results['mean_time']
+                failures[nb_agents] += exp_results['failed_perc']
+                costs_arr.append(exp_results['mean_cost'])
+            else:
+                times2[nb_agents] += exp_results['mean_time']
+
+    return times1[1:] / nb_valid_exp[1:], times2[1:] / nb_valid_exp[1:], nb_agents_arr, costs_arr
 
 
 if __name__ == '__main__':
 
     results = {}
-    timesCBS, failuresCBS, nb_agents_arr, costsCBS = getTimeFailuresArray("saved_dictionary_CBS.pkl")
-    timesPrioritized, failuresPriotized, _, costPrioritized = getTimeFailuresArray("saved_dictionary_Prioritized.pkl")
-    timesDistributed, failuresDistibuted, _, costDistributed = getTimeFailuresArray("saved_dictionary_Distributed.pkl")
+    y1, y2, x, _ = getTimeFailuresArray("saved_dictionary_CBS.pkl")
+    # timesCBS, failuresCBS, nb_agents_arr, costsCBS = getTimeFailuresArray("saved_dictionary_CBS.pkl")
+    # timesPrioritized, failuresPriotized, _, costPrioritized = getTimeFailuresArray("saved_dictionary_Prioritized.pkl")
+    # timesDistributed, failuresDistibuted, _, costDistributed = getTimeFailuresArray("saved_dictionary_Distributed.pkl")
 
-    # print(costsCBS)
-    print(np.mean(costsCBS))
-    print(np.mean(costPrioritized))
-    # print(costPrioritized)
-    print(f"test between CBS and A* {stt.statisticalTests(costsCBS, costPrioritized, significance_lvl=0.05)}")
+    # # print(costsCBS)
+    # print(np.mean(costDistributed))
+    # print(np.mean(costPrioritized))
+    # # print(costPrioritized)
+    # print(f"test between CBS and A* {stt.statisticalTests(costsCBS, costPrioritized, significance_lvl=0.05)}")
 
-    # plt.plot(nb_agents_arr, timesCBS, label="CBS")
-    # plt.plot(nb_agents_arr, timesPrioritized, label="Prioritized")
-    # plt.plot(nb_agents_arr, timesDistributed, label="Distributed")
-    # print(failuresCBS)
+    # # plt.plot(nb_agents_arr, timesCBS, label="CBS")
+    # # plt.plot(nb_agents_arr, timesPrioritized, label="Prioritized")
+    # # plt.plot(nb_agents_arr, timesDistributed, label="Distributed")
+    # # print(failuresCBS)
 
-    plt.plot(nb_agents_arr, failuresCBS, label="CBS")
-    plt.plot(nb_agents_arr, failuresPriotized, label="Prioritized")
-    plt.plot(nb_agents_arr, failuresDistibuted, label="Distributed")
+    # plt.plot(nb_agents_arr, failuresCBS, label="CBS")
+    # plt.plot(nb_agents_arr, failuresPriotized, label="Prioritized")
+    # plt.plot(nb_agents_arr, failuresDistibuted, label="Distributed")
+    plt.plot(x, y1, label="Spawn type 0")
+    plt.plot(x, y2, label="Spawn type 1")
     plt.legend()
     plt.xlabel("Nb of agents")
-    plt.ylabel("Percentage of failures (%)")
+    plt.ylabel("CPU time (s)")
     plt.grid()
     plt.show()
